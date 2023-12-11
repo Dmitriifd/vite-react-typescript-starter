8e8712fb2d8e078d873442f0f8dc06b912700ce0
Dmitriifd <idimka9@gmail.com>
2023-12-11 16:41:12 +0500

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'sonarjs', 'simple-import-sort', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+               // Packages. `react` related packages come first.
+               ["^react"],
+               // Internal packages.
+               ["^(@|@company|@ui|components|utils|config|lib)(/.*|$)", "^@?\\w"],
+               // Side effect imports.
+               ["^\\u0000"],
+               // Parent imports. Put `..` last.
+               ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+               // Other relative imports. Put same-folder imports and `.` last.
+               ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$", "^\\/(?!/?$)"],
+               // Style imports.
+               ["^.+\\.s?css$"],
+            ],
+          },
+        ],
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
