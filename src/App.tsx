b765104dfb97fe1212f174b4b44ce2c2cb8e2f7d
Dmitriifd <idimka9@gmail.com>
2023-12-11 19:03:29 +0500

    added tailwind & shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 
+import { Button } from '@components/ui/button';
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="counter">
-      <h1>Counter</h1>
+    <div className="container flex justify-center items-center gap-3 mt-3">
+      <h1 className="text-3xl font-bold">Counter</h1>
       <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <Button onClick={() => setCount(count + 1)}>Increment</Button>
     </div>
   );
 }
